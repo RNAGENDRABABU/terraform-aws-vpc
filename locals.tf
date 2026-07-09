50fa7e7abb4122ee4f5114983d91f158c5f88d54
Nagendra Babu R <nagu7654@gmail.com>
2026-07-09 20:04:53 +0530

    actions

---
+++ locals.tf
@@ -0,0 +1,15 @@
+locals {
+  common_tags = {
+    Project = var.project
+    Environment = var.environment
+    Terraform = "true"
+  }
+  vpc_final_tags = merge(
+                        local.common_tags,
+                        {
+                            Name = "${var.project}-${var.environment}"
+                        },
+                        var.vpc_tags
+                    )
+                    az_names = slice(data.aws_availability_zones.available.names, 0, 2)
+}
---
+++ variables.tf
@@ -0,0 +1,79 @@
+variable "project" {
+    type = string
+    default = "roboshop"
+}
+
+variable "environment" {
+    type = string
+    default = "dev"
+}
+
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16" 
+}
+
+variable "vpc_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "igw_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "public_subnet_cidrs" {
+    type = list(string)
+    default = ["10.0.0.0/24","10.0.2.0/24"]
+}
+
+variable "public_subnet_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "private_subnet_cidrs" {
+    type = list(string)
+    default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+
+variable "private_subnet_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "database_subnet_cidrs" {
+    type = list(string)
+    default = ["10.0.21.0/24","10.0.22.0/24"]
+}
+
+variable "database_subnet_tags" {
+    type = map(string)
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    default = {}
+    type = map(string)
+}
+
+variable "private_route_table_tags" {
+    default = {}
+    type = map(string)
+}
+
+variable "database_route_table_tags" {
+    default = {}
+    type = map(string)
+}
+
+variable "eip_tags" {
+    default = {}
+    type = map(string)
+}
+
+variable "nat_gateway_tags" {
+    default = {}
+    type = map(string)
+}
