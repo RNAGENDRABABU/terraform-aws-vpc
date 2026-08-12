8e1bbda24205b3e2f57b0bfbc1fe6938985e5365
Nagendra Babu R <nagu7654@gmail.com>
2026-08-12 15:46:08 +0530

    actions

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,6 @@ output "availability_zones" {
 }
 
 output "vpc_id" {
-<<<<<<< HEAD
     value = aws_vpc.main.id
 }
 
@@ -18,19 +17,3 @@ output "private_subnet_ids" {
 output "database_subnet_ids" {
     value = aws_subnet.database[*].id
 }
-=======
-  value = aws_vpc.main.id
-}
-
-output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
-}
-
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
-
-output "database_subnet_ids" {
-  value = aws_subnet.database[*].id
-}
->>>>>>> c38430e9ef18172439a0b8c35230ea9c2c5963e8
